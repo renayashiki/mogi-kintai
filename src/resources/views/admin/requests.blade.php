a62be4f8f595aef56c62092d738fcdc80df72978
takayashiki <07rena01ichika@gmail.com>
2026-02-09 21:53:37 +0900

    A申請一覧　UとCSS分割

--- src/resources/views/admin/requests.blade.php
+++ src/resources/views/admin/requests.blade.php
@@ -3,7 +3,7 @@
 @section('title', '申請一覧')
 
 @section('styles')
-    <link rel="stylesheet" href="{{ asset('css/user/requests.css') }}"> {{-- CSSは使い回し --}}
+    <link rel="stylesheet" href="{{ asset('css/admin/requests.css') }}"> {{-- CSSは使い回し --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
